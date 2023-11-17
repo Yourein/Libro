f93f3a85c09e0b17e74e8c1c6c0a6736f9a950f7
Yourein <rein.you.3.1@gmail.com>
2023-11-17 14:44:09 +0900

    Added application class

---
+++ app/src/main/java/net/yourein/libro/LibroApplication.kt
@@ -0,0 +1,10 @@
+package net.yourein.libro
+
+import android.app.Application
+
+class LibroApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // これ絶対いらない
+    }
+}
