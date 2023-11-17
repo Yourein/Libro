f760623ed47d5623730f981361ea67aeb6439234
Yourein <rein.you.3.1@gmail.com>
2023-11-17 16:09:42 +0900

    Bind entities to RoomDB

--- feature/DataSource/src/main/java/net/yourein/datasource/LibroDB.kt
+++ feature/DataSource/src/main/java/net/yourein/datasource/LibroDB.kt
@@ -4,8 +4,19 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import net.yourein.datasource.entities.Book
+import net.yourein.datasource.entities.BookTag
+import net.yourein.datasource.entities.Series
+import net.yourein.datasource.entities.SeriesBook
+import net.yourein.datasource.entities.Tag
 
-@Database(version = 1)
+@Database(
+    entities = [
+        Book::class,
+        Tag::class, BookTag::class,
+        Series::class, SeriesBook::class,
+        ],
+    version = 1)
 abstract class LibroDB: RoomDatabase() {
     companion object {
         private const val DB_NAME = "libro.db"
