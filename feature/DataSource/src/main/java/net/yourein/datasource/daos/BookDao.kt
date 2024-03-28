cccb33d5db389997389fa5446c78df1ad7332e1d
Yourein <rein.you.3.1@gmail.com>
2024-03-28 19:09:39 +0900

    必要そうなDAOを定義

---
+++ feature/DataSource/src/main/java/net/yourein/datasource/daos/BookDao.kt
@@ -0,0 +1,26 @@
+package net.yourein.datasource.daos
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import net.yourein.datasource.entities.Book
+
+@Dao
+interface BookDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertBook(book: Book)
+
+    @Delete
+    fun deleteBook(book: Book)
+
+    @Query("SELECT * FROM book")
+    fun getAllBook(): List<Book>
+
+    @Query("SELECT * FROM book WHERE isbn=:isbn LIMIT 1")
+    fun getBookByIsbn(isbn: String): Book
+
+    @Query("SELECT * FROM book WHERE name LIKE :name")
+    fun searchBookByName(name: String): List<Book>
+}
