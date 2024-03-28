287477be375920d0c3ef8f38093e8655bcd1771e
Yourein <rein.you.3.1@gmail.com>
2024-03-28 19:07:49 +0900

    Implement ModelState

---
+++ LibroCore/src/main/java/net/yourein/librocore/modelstate/ModelState.kt
@@ -0,0 +1,7 @@
+package net.yourein.librocore.modelstate
+
+sealed class LoadState<out T> {
+    data class Loading<T>(val model: T?) : LoadState<T>()
+    data class Success<T>(val model: T) : LoadState<T>()
+    data class Error<T>(val model: T?, val error: Throwable) : LoadState<T>()
+}
