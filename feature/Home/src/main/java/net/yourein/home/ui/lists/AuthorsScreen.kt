d4483e2192793d0438eaa44b595006d0900a9553
Yourein <rein.you.3.1@gmail.com>
2024-05-22 00:08:19 +0900

    Homeから遷移するリスト画面用のファイルを作成

---
+++ feature/Home/src/main/java/net/yourein/home/ui/lists/AuthorsScreen.kt
@@ -0,0 +1,9 @@
+package net.yourein.home.ui.lists
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AuthorsScreen() {
+    Text(text = "This is Authors")
+}
---
+++ feature/Home/src/main/java/net/yourein/home/ui/lists/SeriesScreen.kt
@@ -0,0 +1,9 @@
+package net.yourein.home.ui.lists
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SeriesScreen() {
+    Text(text = "This is Series")
+}
