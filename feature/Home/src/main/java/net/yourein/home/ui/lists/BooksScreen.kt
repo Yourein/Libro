d7454957801da4dcd5cf8c02a5ce037f70e5ab34
Yourein <rein.you.3.1@gmail.com>
2024-05-30 23:10:51 +0900

    BooksScreenに必要な関数を定義

--- feature/Home/src/main/java/net/yourein/home/ui/lists/BooksScreen.kt
+++ feature/Home/src/main/java/net/yourein/home/ui/lists/BooksScreen.kt
@@ -1,9 +1,47 @@
 package net.yourein.home.ui.lists
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.collections.immutable.ImmutableList
+import net.yourein.datasource.entities.Book
+import net.yourein.home.BooksScreenViewModel
+import net.yourein.librocore.LocalNavigationController
 
 @Composable
-fun BooksScreen() {
-    Text(text = "This is Books")
+fun BooksScreen(
+    viewModel: BooksScreenViewModel = hiltViewModel()
+) {
+    // 後に本のdetailに飛べるようにする
+    val navController = LocalNavigationController.current
+
+    BooksScreen(
+        bookList = viewModel.allBooksList,
+        onBookClicked = { }
+    )
+}
+
+@Composable
+private fun BooksScreen(
+    bookList: ImmutableList<Book>,
+    onBookClicked: (Int) -> Unit,
+) {
+    LazyColumn {
+        items(bookList) { book ->
+            BookItem(book, onBookClicked)
+        }
+    }
+}
+
+@Composable
+private fun BookItem(
+    book: Book,
+    onBookClicked: (Int) -> Unit
+) {
+    Row {
+        Text("TODO")
+    }
 }
